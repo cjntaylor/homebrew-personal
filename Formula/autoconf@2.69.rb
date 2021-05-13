ef940343fb653b741db5e27eb4dc9f858ce77ab8
Colin Taylor <cjntaylor@gmail.com>
2021-05-13 17:10:03 +0000

    autoconf@2.69: add 2.69 bottle.

--- Formula/autoconf@2.69.rb
+++ Formula/autoconf@2.69.rb
@@ -6,6 +6,12 @@ class AutoconfAT269 < Formula
   sha256 "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/cjntaylor/homebrew-personal/releases/download/autoconf@2.69-2.69"
+    sha256 cellar: :any_skip_relocation, catalina:     "35943bb970761e54085ef2838d2f72db0d3378a4d743f8aa72e7a1c700fdce2b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "bc8b79e7c22e97ea90e1727d61b9c2ae0cf750f09b267cb21d2e949cab1b1b16"
+  end
+
   keg_only :versioned_formula
 
   uses_from_macos "m4"
